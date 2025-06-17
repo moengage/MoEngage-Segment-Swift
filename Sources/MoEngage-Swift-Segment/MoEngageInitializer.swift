5622a1c45f03be661b1b0d471ca4267d3155f5db
msoumya-engg-sdk <soumya.mahunt@moengage.com>
2025-06-17 17:39:28 +0530

    MOEN-38247: Added support for file based initialization

--- Sources/MoEngage-Swift-Segment/MoEngageInitializer.swift
+++ Sources/MoEngage-Swift-Segment/MoEngageInitializer.swift
@@ -27,7 +27,22 @@ public final class MoEngageInitializer: NSObject {
 #endif
         trackPluginTypeAndVersion(sdkConfig: sdkConfig)
     }
-    
+
+    /// Method to initialize the default instance of MoEngageSDK
+    /// with data from Info.plist.
+    @objc public static func initializeDefaultInstance() {
+        MoEngage.sharedInstance.initializeDefaultInstance()
+
+        guard
+            let sdkConfig = try? MoEngageInitialization.fetchSDKConfigurationFromInfoPlist(),
+            !sdkConfig.appId.isEmpty
+        else {
+            MoEngageLogger.logDefault(message: "App ID is empty. Please provide a valid App ID to setup the SDK.")
+            return
+        }
+        trackPluginTypeAndVersion(sdkConfig: sdkConfig)
+    }
+
     /// Method to initialize the other instance of MoEngageSDK
     /// - Parameter sdkConfig: MoEngageSDKConfig
     @objc public static func initializeInstance(sdkConfig: MoEngageSDKConfig) {
